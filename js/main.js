49a2d13ea3ef2bd50150be7d5c0588b2b4dde1bc
JoelB-E276 <joel.beauroy@outlook.fr>
2021-04-13 14:10:40 +0200

    Change test

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Change card1  test 
+document.getElementById("card1").addEventListener("click", function changeaCard() {
+document.getElementById("card1").style.backgroundColor = "black";
+}); 
+
